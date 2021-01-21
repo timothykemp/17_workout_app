e72d0a9182a476e59b4fe09f6d1368bab2af09ab
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-01-20 18:36:11 -0600

    initial file creation and model creation

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+let Workout = require("./models/workout");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Allows req.body to actually work
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.get("/api/workouts", (req, res) => {
+    Workout.
+})
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
