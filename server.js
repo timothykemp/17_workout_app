952f5a97c8884b3782ba6ab654a838c4209953ef
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-02-01 18:53:04 -0600

    updated paths on html files to point to more organized public folder. created routes file

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-let Workout = require("./models/workout");
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,16 +13,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-app.get("/api/workouts", (req, res) => {
-    Workout.find({})
-        .then(workout => {
-            res.json(workout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-})
-
+app.use(require("./routes/index.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
